8793352ac2798134aa5cfcd41e526bcb65994a29
juntang <juntang@ipagwstn6.med.yale.internal>
2020-11-30 11:42:58 -0500

    initial

---
+++ run_adabelief_walker2d.sh
@@ -0,0 +1,10 @@
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 0 --gpu 0  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 1 --gpu 0  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 2 --gpu 0  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 3 --gpu 0  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 4 --gpu 0  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 5 --gpu 1  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 6 --gpu 1  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 7 --gpu 1  &
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 8 --gpu 1  & 
+python train_sac_optim2.py --optimizer OfficialAdaBelief --steps 3000000 --eval-interval 50000 --env Walker2d-v2 --seed 9 --gpu 1  &
